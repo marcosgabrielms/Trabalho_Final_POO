4d34659a940e75cd605a8cb7696bbba53d19fd6f
Marcos Gabriel <marcosgabriel166@gmail.com>
2025-12-13 18:56:45 -0300

    :bug: fix: corrige pequenos erros de lógica de código

--- src/modelos/Personagem.ts
+++ src/modelos/Personagem.ts
@@ -37,7 +37,7 @@ export abstract class Personagem {
     public receberDanoVerdadeiro(valor: number): void {
         console.log(`   ⚡ DANO VERDADEIRO (Ignorou Defesa)`); 
         this._vida -= valor;
-        if(this._vida < 0) this._vida - 0;
+        if(this._vida < 0) this._vida = 0;
         const percentual = Math.round((this._vida / this._vidaMaxima) * 100);
         console.log(`   💥 ${this._nome} tomou ${valor.toFixed(0)} de dano.`);
         console.log(`   ❤️  Status: ${this._vida}/${this._vidaMaxima} PV (${percentual}%)`);
