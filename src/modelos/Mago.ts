cb1b5ba3ac5648d2ee560a787a9e3b839c2c66a5
marcosgabrielms <marcosgabriel166@gmail.com>
2025-12-15 11:26:04 -0300

    :bug: fix(mago):  Corrige lógica para classe 'Mago' não ficar com vida negativa ao atacar.

--- src/modelos/Mago.ts
+++ src/modelos/Mago.ts
@@ -15,7 +15,12 @@ export class Mago extends Personagem {
         console.log(`\n✨ ${this.nome} Conjura uma magia em ${alvo.nome}!`);
         
         this._vida -= 10;
-        console.log(`   💧 ${this._nome} gastou 10 PV para usar magia.`);
+        
+        if (this._vida < 0) {
+            this._vida = 0;
+        }
+
+        console.log(`   💧 ${this.nome} gastou 10 PV para usar magia.`);
 
         if (alvo instanceof Arqueiro) {
             valorAtaque = valorAtaque * 2;
@@ -34,7 +39,7 @@ export class Mago extends Personagem {
         const acao = new Acao(idAcao, this.nome, alvo.nome, msg, valorAtaque);
         this.registrarAcao(acao);
 
-        this.registrarAcao(new Acao(idAcao+1, this.nome, this.nome, "Custo de Mana", 10));
+        this.registrarAcao(new Acao(idAcao + 1, this.nome, this.nome, "Custo de Mana", 10));
 
         return acao;
     }
